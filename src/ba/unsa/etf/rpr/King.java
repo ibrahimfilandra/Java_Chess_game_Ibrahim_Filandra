64d90085523455f80e26377bdd7d6e7e8039f6b0
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-03 21:12:37 +0100

    zavrsen konstruktor u klasi King

--- src/ba/unsa/etf/rpr/King.java
+++ src/ba/unsa/etf/rpr/King.java
@@ -7,6 +7,13 @@ public class King extends ChessPiece {
     String position;
     Color color;
     King (String position, Color color){
+        if(position.charAt(0)<'a'){
+            if(position.charAt(0)<'A' || position.charAt(0)>'H')throw new IllegalArgumentException();
+        }
+        if(position.charAt(0)>'h')throw new IllegalArgumentException();   //Je li neispravno navedena kolona
+
+        if(position.charAt(1)<'1'||position.charAt(1)>'8') throw new IllegalArgumentException(); //je li neispravno naveden red
+        if(position.length()!=2) throw new IllegalArgumentException(); //Finalna provjera ispravnosti formata
         this.position=position;
         this.color=color;
     }
@@ -22,14 +29,14 @@ public class King extends ChessPiece {
 if(position.charAt(0)<'a'){
     if(position.charAt(0)<'A' || position.charAt(0)>'H')throw new IllegalArgumentException();
 }
-if(position.charAt(0)>'h')throw new IllegalArgumentException();   //Je li neispravno navedena kolona
+if(position.charAt(0)>'h')throw new IllegalArgumentException();
 
-        if(position.charAt(1)<'1'||position.charAt(1)>'8') throw new IllegalArgumentException(); //je li neispravno naveden red
-        if(position.length()!=2) throw new IllegalArgumentException(); //Finalna provjera ispravnosti formata
+        if(position.charAt(1)<'1'||position.charAt(1)>'8') throw new IllegalArgumentException();
+        if(position.length()!=2) throw new IllegalArgumentException();
 
         if(abs(this.getPosition().charAt(1)-position.charAt(1))>1) throw new IllegalChessMoveException();
         if(abs(this.getPosition().charAt(0)-position.charAt(0))>1  && abs(this.getPosition().charAt(0) -position.charAt(0))!=33)
-            throw new IllegalChessMoveException();       //Bacanje izuzetka ako se zeli napraviti ilegalno kretanje sa kraljem
+            throw new IllegalChessMoveException();
 
 
     }
