e5f283a9d432a1bee81388a4b0cf41d74d58e995
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-03 21:11:05 +0100

    Uradjen move() u klasi King te deklarisan IllegalChessMoveException

--- src/ba/unsa/etf/rpr/ChessPiece.java
+++ src/ba/unsa/etf/rpr/ChessPiece.java
@@ -16,17 +16,11 @@ public abstract class ChessPiece {
         return color;
     }
 
+
+
     void Move(String position) {
 
-        if(position.charAt(0)!='A' || position.charAt(0)!='B'|| position.charAt(0)!='C'|| position.charAt(0)!='D'||
-        position.charAt(0)!='E'|| position.charAt(0)!='F' || position.charAt(0)!='G'|| position.charAt(0)!='H'
-        || position.charAt(0)!='a'|| position.charAt(0)!='b'|| position.charAt(0)!='c'|| position.charAt(0)!='d'
-                || position.charAt(0)!='e'|| position.charAt(0)!='f'|| position.charAt(0)!='g'|| position.charAt(0)!='h')
-        throw new IllegalArgumentException();
 
-        if(position.charAt(1)!='1'||position.charAt(1)!='2'||position.charAt(1)!='3' ||position.charAt(1)!='4'
-        ||position.charAt(1)!='5'||position.charAt(1)!='6'||position.charAt(1)!='7'||position.charAt(1)!='8'
-        ) throw new IllegalArgumentException();
 
     }
 
--- src/ba/unsa/etf/rpr/King.java
+++ src/ba/unsa/etf/rpr/King.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr;
 
+import static java.lang.Math.abs;
+
 public class King extends ChessPiece {
 
     String position;
@@ -9,4 +11,26 @@ public class King extends ChessPiece {
         this.color=color;
     }
 
+    public class IllegalChessMoveException extends RuntimeException {
+   public IllegalChessMoveException() {super();}
+
+    }
+    @Override
+    void Move(String position) {
+
+
+if(position.charAt(0)<'a'){
+    if(position.charAt(0)<'A' || position.charAt(0)>'H')throw new IllegalArgumentException();
+}
+if(position.charAt(0)>'h')throw new IllegalArgumentException();   //Je li neispravno navedena kolona
+
+        if(position.charAt(1)<'1'||position.charAt(1)>'8') throw new IllegalArgumentException(); //je li neispravno naveden red
+        if(position.length()!=2) throw new IllegalArgumentException(); //Finalna provjera ispravnosti formata
+
+        if(abs(this.getPosition().charAt(1)-position.charAt(1))>1) throw new IllegalChessMoveException();
+        if(abs(this.getPosition().charAt(0)-position.charAt(0))>1  && abs(this.getPosition().charAt(0) -position.charAt(0))!=33)
+            throw new IllegalChessMoveException();       //Bacanje izuzetka ako se zeli napraviti ilegalno kretanje sa kraljem
+
+
+    }
 }
