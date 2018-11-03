aa74580208b2a0f84ca7fdf1212667e66066ff5a
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-03 22:31:28 +0100

    Queen popravio move, sad je definisano da se moze kretati samo horizontalno, vertikalno i dijagonalno - a ne bilo kako

--- src/ba/unsa/etf/rpr/Queen.java
+++ src/ba/unsa/etf/rpr/Queen.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr;
 
+import static java.lang.Math.abs;
+
 public class Queen extends ChessPiece {
 
     Queen (String position, Color color) {
@@ -15,7 +17,7 @@ public class Queen extends ChessPiece {
 
     }
 
-    
+
     void Move(String position) {
         if(position.charAt(0)<'a'){
             if(position.charAt(0)<'A' || position.charAt(0)>'H')throw new IllegalArgumentException();
@@ -25,6 +27,9 @@ public class Queen extends ChessPiece {
         if(position.charAt(1)<'1'||position.charAt(1)>'8') throw new IllegalArgumentException();
         if(position.length()!=2) throw new IllegalArgumentException();
 
+        if (!(position.charAt(0)==this.position.charAt(0) || this.position.charAt(1)==position.charAt(1) || (abs(this.position.charAt(0)
+        -position.charAt(0))==abs(this.position.charAt(1)-position.charAt(1))) )) throw new IllegalChessMoveException();
+        //ako se ne krece u istom redu ili u istoj koloni ili dijagonalno, baci izuzetak
         this.position=position;
     }
 
