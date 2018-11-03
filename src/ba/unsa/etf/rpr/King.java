cb3887abcfae315ba87211a896905254e5335de0
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-03 18:30:51 +0100

    Zapoceta klasa King. Uradjen konstruktor

---
+++ src/ba/unsa/etf/rpr/King.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr;
+
+public class King extends ChessPiece {
+
+    String position;
+    Color color;
+    King (String position, Color color){
+        this.position=position;
+        this.color=color;
+    }
+
+}
