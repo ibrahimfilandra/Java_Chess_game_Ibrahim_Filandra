752709b97cffb0364ca9876de7fe90bd53a586aa
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-03 23:50:57 +0100

    Knight zavrsen move

--- src/ba/unsa/etf/rpr/Knight.java
+++ src/ba/unsa/etf/rpr/Knight.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr;
 
+import static java.lang.Math.abs;
+
 public class Knight extends ChessPiece {
 
     Knight(String position, Color color) {
@@ -18,5 +20,19 @@ public class Knight extends ChessPiece {
 
     }
 
+    void move(String position) {
+        if(position.charAt(0)<'a'){
+            if(position.charAt(0)<'A' || position.charAt(0)>'H')throw new IllegalArgumentException();
+        }
+        if(position.charAt(0)>'h')throw new IllegalArgumentException();
+
+        if(position.charAt(1)<'1'||position.charAt(1)>'8') throw new IllegalArgumentException();
+        if(position.length()!=2) throw new IllegalArgumentException();
+
+        if (!( abs(position.charAt(0)-this.position.charAt(0))==3 &&  abs(position.charAt(1)-this.position.charAt(1))==1 ) ||
+        ( abs(position.charAt(1)-this.position.charAt(1))==3 &&  abs(position.charAt(0)-this.position.charAt(0))==1 ) ||
+                ( abs(position.charAt(0)-this.position.charAt(0))==35 &&  abs(position.charAt(1)-this.position.charAt(1))==1 ))
+            throw new IllegalChessMoveException();
+    }
 
 }
