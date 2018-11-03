86350a4d8ff33096cdc321755b02acf4b8902cbf
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-04 00:01:54 +0100

    Pawn zavrsen move(). Uradjeno je da se samo moze kretati za 1 polje naprijed, treba provjeriti sta za situaciju kada "jede" drugu figuru

--- src/ba/unsa/etf/rpr/Pawn.java
+++ src/ba/unsa/etf/rpr/Pawn.java
@@ -1,5 +1,5 @@
 package ba.unsa.etf.rpr;
-
+import static java.lang.Math.abs;
 public class Pawn extends ChessPiece{
 
     Pawn(String position, Color color) {
@@ -13,6 +13,21 @@ public class Pawn extends ChessPiece{
         if(position.length()!=2) throw new IllegalArgumentException(); //Finalna provjera ispravnosti formata
         this.position=position;
         this.color=color;
+    }
+
+    void move(String position) {
+
+        if(position.charAt(0)<'a'){
+            if(position.charAt(0)<'A' || position.charAt(0)>'H')throw new IllegalArgumentException();
+        }
+        if(position.charAt(0)>'h')throw new IllegalArgumentException();
+
+        if(position.charAt(1)<'1'||position.charAt(1)>'8') throw new IllegalArgumentException();
+        if(position.length()!=2) throw new IllegalArgumentException();
+
+        if   (!( (position.charAt(0)==this.position.charAt(0) || abs(this.position.charAt(0)-position.charAt(0))==32  )&& position.charAt(1)-this.position.charAt(1)==1 ))
+          throw new IllegalChessMoveException(); //samo se moze kretati naprijed za 1 polje
+        this.position=position;
     }
 
 
