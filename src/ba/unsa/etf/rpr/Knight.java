409f68737bd92082b7baa1e8fbc3b3a27fc8e161
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-03 23:02:19 +0100

    Knight zavrsen konstruktor

---
+++ src/ba/unsa/etf/rpr/Knight.java
@@ -0,0 +1,22 @@
+package ba.unsa.etf.rpr;
+
+public class Knight extends ChessPiece {
+
+    Knight(String position, Color color) {
+
+        if(position.charAt(0)<'a'){
+            if(position.charAt(0)<'A' || position.charAt(0)>'H')throw new IllegalArgumentException();
+        }
+        if(position.charAt(0)>'h')throw new IllegalArgumentException();   //Je li neispravno navedena kolona
+
+        if(position.charAt(1)<'1'||position.charAt(1)>'8') throw new IllegalArgumentException(); //je li neispravno naveden red
+        if(position.length()!=2) throw new IllegalArgumentException(); //Finalna provjera ispravnosti formata
+        this.position=position;
+        this.color=color;
+
+
+
+    }
+
+
+}
