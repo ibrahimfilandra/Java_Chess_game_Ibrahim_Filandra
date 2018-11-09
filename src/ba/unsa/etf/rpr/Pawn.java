1baacba6f1e6eb5de38d88c7732a2b3c714c42cf
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-09 17:02:00 +0100

    dodano da se pijun moze kretati za 2 mjesta ako polazi sa pocetne pozicije. Iskljucena mogucnost da moze ici dijagonalno, to cu posebno implementirati da moze ici dijagonalno ako jede drugu figuru

--- src/ba/unsa/etf/rpr/Pawn.java
+++ src/ba/unsa/etf/rpr/Pawn.java
@@ -25,8 +25,14 @@ public class Pawn extends ChessPiece{
         if(position.charAt(1)<'1'||position.charAt(1)>'8') throw new IllegalArgumentException();
         if(position.length()!=2) throw new IllegalArgumentException();
 
-        if   (!( (position.charAt(0)==this.position.charAt(0)||abs(position.charAt(0)-this.position.charAt(0))==1 || abs(this.position.charAt(0)-position.charAt(0))==32|| abs(this.position.charAt(0)-position.charAt(0))==31
-        ||abs(this.position.charAt(0)-position.charAt(0))==33)&& position.charAt(1)-this.position.charAt(1)==1 ))
+        if(this.color==Color.WHITE && this.position.charAt(1)=='2'){
+            if(position.charAt(1)==4 && this.position.charAt(0)==position.charAt(0)) this.position=position;
+        }
+       else if(this.color==Color.BLACK && this.position.charAt(1)=='7'){
+            if(position.charAt(1)==5 && this.position.charAt(0)==position.charAt(0)) this.position=position;
+        }
+
+      else if   (!( (position.charAt(0)==this.position.charAt(0) || abs(this.position.charAt(0)-position.charAt(0))==32)&& position.charAt(1)-this.position.charAt(1)==1 ))
           throw new IllegalChessMoveException(); //samo se moze kretati naprijed za 1 polje i dijagonalno naprijed za 1 polje
         this.position=position;
     }
