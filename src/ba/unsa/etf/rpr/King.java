9ad52a20bc7ccf76e4fdee98026bd4d527804b44
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-09 17:40:31 +0100

    popravljen redoslijed uslova. prvo treba ispitivati da li je position string od 2 chara

--- src/ba/unsa/etf/rpr/King.java
+++ src/ba/unsa/etf/rpr/King.java
@@ -5,13 +5,14 @@ import static java.lang.Math.abs;
 public class King extends ChessPiece {
 
     King (String position, Color color){
+        if(position.length()!=2) throw new IllegalArgumentException();
         if(position.charAt(0)<'a'){
             if(position.charAt(0)<'A' || position.charAt(0)>'H')throw new IllegalArgumentException();
         }
         if(position.charAt(0)>'h')throw new IllegalArgumentException();   //Je li neispravno navedena kolona
 
         if(position.charAt(1)<'1'||position.charAt(1)>'8') throw new IllegalArgumentException(); //je li neispravno naveden red
-        if(position.length()!=2) throw new IllegalArgumentException(); //Finalna provjera ispravnosti formata
+         //Finalna provjera ispravnosti formata
         this.position=position;
         this.color=color;
     }
--- src/ba/unsa/etf/rpr/Pawn.java
+++ src/ba/unsa/etf/rpr/Pawn.java
@@ -25,16 +25,15 @@ public class Pawn extends ChessPiece{
         if(position.charAt(1)<'1'||position.charAt(1)>'8') throw new IllegalArgumentException();
         if(position.length()!=2) throw new IllegalArgumentException();
 
-        if(this.color==Color.WHITE && this.position.charAt(1)=='2'){
-            if(position.charAt(1)==4 && this.position.charAt(0)==position.charAt(0)) this.position=position;
-        }
-       else if(this.color==Color.BLACK && this.position.charAt(1)=='7'){
-            if(position.charAt(1)==5 && this.position.charAt(0)==position.charAt(0)) this.position=position;
-        }
+        if(this.color==Color.WHITE && this.position.charAt(1)=='2'&& position.charAt(1)=='4' && this.position.charAt(0)==position.charAt(0))
+            this.position=position;
 
-      else if   ( (position.charAt(0)==this.position.charAt(0) || abs(this.position.charAt(0)-position.charAt(0))==32)&& position.charAt(1)-this.position.charAt(1)==1 )
+       else if(this.color==Color.BLACK && this.position.charAt(1)=='7'&&position.charAt(1)=='5' && this.position.charAt(0)==position.charAt(0))
+             this.position=position;
+
+       else if   ( (position.charAt(0)==this.position.charAt(0) || abs(this.position.charAt(0)-position.charAt(0))==32)&& position.charAt(1)-this.position.charAt(1)==1 )
          this.position=position;
-          else throw new IllegalChessMoveException(); //samo se moze kretati naprijed za 1 polje i dijagonalno naprijed za 1 polje
+          else throw new IllegalChessMoveException(); //samo se moze kretati naprijed za 1 polje ili 2 ako mu je prvi potez
 
     }
 
