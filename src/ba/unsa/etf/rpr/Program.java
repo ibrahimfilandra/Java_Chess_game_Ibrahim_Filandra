2fd1fc63ff155bc7c3a011ae9439bcbebb65ff63
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-11-10 20:42:32 +0100

    dodan i unos za crnog igraca

--- src/ba/unsa/etf/rpr/Program.java
+++ src/ba/unsa/etf/rpr/Program.java
@@ -15,7 +15,7 @@ public class Program {
         igra:
         while (true) {
             do {
-                System.out.println("White move:");
+                System.out.println("White move: ");
                 s1 = scan.next();
                 if(s1.charAt(0)=='X' && s1.length()==1) {
                     System.out.println("White has resigned");
@@ -76,6 +76,67 @@ if(!ispravan) System.out.println("Illegal move");
             } while (!ispravan);
             if(b.isCheck(ChessPiece.Color.WHITE)) System.out.println("White is in check");
 
+
+            do {
+                System.out.println("Black move: ");
+                s2 = scan.next();
+                if(s2.charAt(0)=='X' && s2.length()==1) {
+                    System.out.println("Black has resigned");
+                    break igra;
+                }
+                if (s2.charAt(0) > 'a' && s2.charAt(0) < 'h') {
+                    if (s2.charAt(1) > '1' && s2.charAt(1) < '9') {
+                        b.move(Pawn.class, ChessPiece.Color.BLACK, s2);
+                        ispravan = true;
+                    }else ispravan=false;
+                }
+                else if (s2.charAt(0) == 'N') {
+                    if(s2.charAt(1)>'a' && s2.charAt(1)<'h'){
+                        if(s2.charAt(2)>'1' && s2.charAt(2)<'9'){
+                            b.move(Knight.class, ChessPiece.Color.BLACK, (char)(s2.charAt(1))+""+(char)(s2.charAt(2)) );
+                            ispravan=true;
+                        } else ispravan=false;
+                    } else ispravan=false;
+                }
+
+                else if(s2.charAt(0)=='R'){
+                    if(s2.charAt(1)>'a' && s2.charAt(1)<'h'){
+                        if(s2.charAt(2)>'1' && s2.charAt(2)<'9'){
+                            b.move(Rook.class, ChessPiece.Color.BLACK, (char)(s2.charAt(1))+""+(char)(s2.charAt(2)));
+                            ispravan=true;
+                        }else ispravan=false;
+                    }else ispravan=false;
+                }
+
+                else if(s2.charAt(0)=='K'){
+                    if(s2.charAt(1)>'a' && s2.charAt(1)<'h'){
+                        if(s2.charAt(2)>'1' && s2.charAt(2)<'9'){
+                            b.move(King.class, ChessPiece.Color.BLACK, (char)(s2.charAt(1))+""+(char)(s2.charAt(2)));
+                            ispravan=true;
+                        }else ispravan=false;
+                    }else ispravan=false;
+                }
+
+                else if(s2.charAt(0)=='Q'){
+                    if(s2.charAt(1)>'a' && s2.charAt(1)<'h'){
+                        if(s2.charAt(2)>'1' && s2.charAt(2)<'9'){
+                            b.move(Queen.class, ChessPiece.Color.BLACK, (char)(s2.charAt(1))+""+(char)(s2.charAt(2)));
+                            ispravan=true;
+                        }else ispravan=false;
+                    }else ispravan=false;
+                }
+
+                else if(s2.charAt(0)=='B'){
+                    if(s2.charAt(1)>'a' && s2.charAt(1)<'h'){
+                        if(s2.charAt(2)>'1' && s2.charAt(2)<'9'){
+                            b.move(Bishop.class, ChessPiece.Color.WHITE, (char)(s2.charAt(1))+""+(char)(s2.charAt(2)));
+                            ispravan=true;
+                        }else ispravan=false;
+                    }else ispravan=false;
+                }
+                else ispravan=false;
+                if(!ispravan) System.out.println("Illegal move");
+            } while (!ispravan);
 
         }
     }
